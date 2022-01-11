a510beb9a188370944055b66740dcea4d3565977
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2022-01-11 15:47:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/Messi12300/VCPlayerBot-1 /VCPlayerBot-1
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/Messi12300/VCPlayerBot-1 -b $BRANCH /VCPlayerBot-1
 fi
-cd /VCPlayerBot
+cd /VCPlayerBot-1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
